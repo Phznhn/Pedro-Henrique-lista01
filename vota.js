e6c0a30bfbbd3e69e7f09070b6565bc902bcaa32
Phznhn <pedro12cabalheiro@gmail.com>
2026-08-24 16:45:25 -0400

    questao resolvida

--- vota.js
+++ vota.js
@@ -1,1 +1,13 @@
+let nome = "joao";
 
+let idade = 15;
+
+function votaounao (nome,idade){
+    if (idade >= 16){
+        console.log(nome +" pode votar meu parceiro");
+    } else {
+        console.log(nome +" não vota meu amigo");
+    }
+}
+
+votaounao(nome, idade);
