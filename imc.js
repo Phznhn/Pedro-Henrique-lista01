5c0102b7097d5c2643c85a887ec5ff74cea44648
Phznhn <pedro12cabalheiro@gmail.com>
2026-08-26 19:53:52 -0400

    resolvido

--- imc.js
+++ imc.js
@@ -1,1 +1,14 @@
+let peso = 70;
 
+let altura = 1.79;
+
+function imc(peso, altura){
+    let conta = peso/(altura*altura);
+    
+    if (conta >18.5 && conta<24.9){
+        console.log(conta + " peso e altura normal");
+    } else {
+        console.log(conta + "tem que se cuidar meu amigo");
+    }
+}
+imc(peso, altura);
