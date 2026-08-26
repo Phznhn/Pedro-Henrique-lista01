9ea8fc7e1aab03c1dbac82f847f68f66daaa3933
Phznhn <pedro12cabalheiro@gmail.com>
2026-08-26 19:43:26 -0400

    resolvido

--- crescente.js
+++ crescente.js
@@ -1,1 +1,9 @@
+var numerinhos = [22, 13, 14];
 
+function ordenador(numerinhos){
+    numerinhos.sort();
+    return numerinhos;
+}
+
+let resultado = ordenador(numerinhos);
+console.log(resultado);
