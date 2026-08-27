e323e1d317ebd911bc234753acb3f3018d82dc37
Phznhn <pedro12cabalheiro@gmail.com>
2026-08-26 21:14:45 -0400

    resolvido

--- maior_menor.js
+++ maior_menor.js
@@ -1,1 +1,17 @@
+var numero = [7,8,9,1,4];
 
+var maior = numero[0];
+
+var menor = numero[0];
+
+for (var i=0;i<5;i++){
+    if (numero[i]>maior){
+        maior = numero[i];
+    }
+   if (numero[i]<menor){
+        menor = numero[i];
+    }   
+}
+
+console.log("o maior e:"+maior);
+console.log("o menor e:"+menor);
