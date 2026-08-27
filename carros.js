881acd79c3413cdeea65e2b7ae0de8ee9d909240
Phznhn <pedro12cabalheiro@gmail.com>
2026-08-26 21:21:52 -0400

    resolvido

--- carros.js
+++ carros.js
@@ -1,1 +1,10 @@
-
+var carros = [
+    { marca: "toyota", modelo: "corolla", ano: 2014 },
+    { marca: "honda", modelo: "civic", ano: 2019 },
+    { marca: "ford", modelo: "focus", ano: 2021 }
+];
+for (var i=0;i<3;i++){
+    if (carros[i].ano>2015){
+        console.log(carros[i].marca+" "+carros[i].modelo+" "+carros[i].ano);
+}
+}
