2f09950955e2799a4be2402f61fd3160de1a8b35
Phznhn <pedro12cabalheiro@gmail.com>
2026-08-26 20:04:29 -0400

    resolvido

--- media.js
+++ media.js
@@ -1,1 +1,14 @@
+var numero = [7,5,3,9,7];
 
+function medianumero(numero){
+    let soma=0
+    
+    for (let i=0;i<5;i++){
+        soma+=numero[i];
+    }
+    let media= soma/5
+    return media;
+}
+
+let resultado = medianumero(numero);
+console.log("a media dos numeros e: " + resultado);
